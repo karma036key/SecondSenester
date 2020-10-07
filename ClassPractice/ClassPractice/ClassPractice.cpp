b928c324be0f48d34e73d3b1dac309013ac7c9e5
Atsushi-oogami <57586505+karma036key@users.noreply.github.com>
2020-10-07 14:24:34 +0900

    リファクタリング作業　その１ 　・クラス名の変更 　・クラスの宣言、定義部分をファイルに分割 　・共通ヘッダーをCommon.hに移動

--- ClassPractice/ClassPractice/ClassPractice.cpp
+++ ClassPractice/ClassPractice/ClassPractice.cpp
@@ -1,54 +1,20 @@
-﻿#include <stdlib.h>
-#include <stdio.h>
+﻿#include "Common.h"
+#include "Unit.h"
 
-class Fighter
-{
-public:
-    int m_hp ;
-    int m_str;
-    int m_mag;
-    int m_tec;
-    int m_spd;
-    int m_def;
-    int m_luc;
-    int m_mde;
-
-public:     
-    Fighter();
-    ~Fighter();
-};
 
 int main()
 {
-    Fighter fighter;
+    Unit unit;
 
-    printf(" m_hp  = %d\n", fighter.m_hp);
-    printf(" m_str = %d\n", fighter.m_hp);
-    printf(" m_mag = %d\n", fighter.m_hp);
-    printf(" m_tec = %d\n", fighter.m_hp);
-    printf(" m_spd = %d\n", fighter.m_hp);
-    printf(" m_def = %d\n", fighter.m_hp);
-    printf(" m_luc = %d\n", fighter.m_hp);
-    printf(" m_mde = %d\n", fighter.m_hp);
+    printf(" m_hp  = %d\n", unit.m_hp);
+    printf(" m_str = %d\n", unit.m_hp);
+    printf(" m_mag = %d\n", unit.m_hp);
+    printf(" m_tec = %d\n", unit.m_hp);
+    printf(" m_spd = %d\n", unit.m_hp);
+    printf(" m_def = %d\n", unit.m_hp);
+    printf(" m_luc = %d\n", unit.m_hp);
+    printf(" m_mde = %d\n", unit.m_hp);
 
     system("pause");
     return 0;
 }
-
- Fighter::Fighter()
-{
-     m_hp  = 0;
-     m_str = 0;
-     m_mag = 0;
-     m_tec = 0;
-     m_spd = 0;
-     m_def = 0;
-     m_luc = 0;
-     m_mde = 0;
-}
-
- Fighter::~Fighter()
-{
-}
-
-
---
+++ ClassPractice/ClassPractice/Unit.h
@@ -0,0 +1,22 @@
+
+#ifndef UNIT_H
+#define UNIT_H
+
+class Unit
+{
+public:
+    int m_hp;
+    int m_str;
+    int m_mag;
+    int m_tec;
+    int m_spd;
+    int m_def;
+    int m_luc;
+    int m_mde;
+
+public:
+    Unit();
+    ~Unit();
+};
+
+#endif // #ifndef UNIT_H
