4f541dce1114bb3603c3c32c696c2415449f5d59
Atsushi-oogami <57586505+karma036key@users.noreply.github.com>
2020-10-07 13:38:16 +0900

    クラスの作り方練習用のプロジェクト　作成

---
+++ ClassPractice/ClassPractice/ClassPractice.cpp
@@ -0,0 +1,54 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+class Fighter
+{
+public:
+    int m_hp ;
+    int m_str;
+    int m_mag;
+    int m_tec;
+    int m_spd;
+    int m_def;
+    int m_luc;
+    int m_mde;
+
+public:     
+    Fighter();
+    ~Fighter();
+};
+
+int main()
+{
+    Fighter fighter;
+
+    printf(" m_hp  = %d\n", fighter.m_hp);
+    printf(" m_str = %d\n", fighter.m_hp);
+    printf(" m_mag = %d\n", fighter.m_hp);
+    printf(" m_tec = %d\n", fighter.m_hp);
+    printf(" m_spd = %d\n", fighter.m_hp);
+    printf(" m_def = %d\n", fighter.m_hp);
+    printf(" m_luc = %d\n", fighter.m_hp);
+    printf(" m_mde = %d\n", fighter.m_hp);
+
+    system("pause");
+    return 0;
+}
+
+ Fighter::Fighter()
+{
+     m_hp  = 0;
+     m_str = 0;
+     m_mag = 0;
+     m_tec = 0;
+     m_spd = 0;
+     m_def = 0;
+     m_luc = 0;
+     m_mde = 0;
+}
+
+ Fighter::~Fighter()
+{
+}
+
+
