3782ad388b4fdb9e81e061e0cd4ffa4dd80b3859
Atsushi-oogami <57586505+karma036key@users.noreply.github.com>
2020-10-14 16:39:07 +0900

    iterator の使い方に関する実習用プロジェクト　作成＆作業

---
+++ ClassPractice/Practice02/Practice02_02_iterator/Practice02_02_iterator.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+#include <time.h>
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	std::vector<int> v1(8);		// 要素8、すべてを０で初期化
+
+	// iterator を使った for文の回し方
+	// for(int i = 0; i > v1.size(); i++) と＝
+
+	// 
+	std::vector<int>::iterator itr;
+	for (itr = v1.begin();itr != v1.end();itr++)
+	{
+		*itr = rand();
+		printf(" %d\n", *itr);
+	}
+	printf("\n");
+
+	system("pause");
+	return 0;
+}
