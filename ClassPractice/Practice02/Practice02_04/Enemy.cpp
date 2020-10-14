df316b8012208a3d764955a4e2eae734b9df077d
Atsushi-oogami <57586505+karma036key@users.noreply.github.com>
2020-10-14 17:31:47 +0900

    Enemyクラス　作成

---
+++ ClassPractice/Practice02/Practice02_04/Enemy.cpp
@@ -0,0 +1,56 @@
+#include "Common.h"
+#include "Enemy.h"
+
+Enemy::Enemy()
+{
+	int		m_Hp		= 0;
+	float	m_PosX		= 0;
+	float	m_PosY		= 0;
+	float	m_MoveSpeed	= 0;
+}
+Enemy::~Enemy()
+{
+
+}
+
+void Enemy::Exec()
+{
+
+}
+void Enemy::Draw()
+{
+
+}
+
+void Enemy::SetHp(int hp)
+{
+	m_Hp = hp;
+}
+void Enemy::SetPos(float x, float y)
+{
+	m_PosX = x;
+	m_PosY = y;
+}
+void Enemy::SetMoveSpeed(float speed)
+{
+	m_MoveSpeed = speed;
+}
+
+
+int Enemy::GetHp()
+{
+	return m_Hp;
+}
+float Enemy::GetPosX()
+{
+	return m_PosX;
+}
+float Enemy::GetPosY()
+{
+	return m_PosY;
+}
+float Enemy::GetMoveSpeed()
+{
+	return m_MoveSpeed;
+}
+
