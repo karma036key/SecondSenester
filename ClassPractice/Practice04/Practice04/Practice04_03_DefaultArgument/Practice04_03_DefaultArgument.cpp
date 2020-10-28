14401c698d586b8e8943fc5aea79c2b1dd51f619
Atsushi-oogami <57586505+karma036key@users.noreply.github.com>
2020-10-28 17:28:55 +0900

    第4回　その３ 関数のデフォルト引数
    書き方・使い方
    デフォルト引数に関する注意点
    使用例

---
+++ ClassPractice/Practice04/Practice04/Practice04_03_DefaultArgument/Practice04_03_DefaultArgument.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+// デフォルト引数
+// 関数の引数にはデフォルト値が設定 可
+// 
+class Calculator
+{
+public:
+    int Add(int a, int b);
+    // 引数b のデフォルト値 ２とする
+    // 戻り値の型　関数(仮引数名 = 初期値)
+    int Mul(int a, int b = 2);
+
+    // 注 : デフォルト値設定　　宣言・定義どちらでも 可
+    //      ただし、両方に書くことはできない                // "オススメ" .h 側(宣言)に書き定義部にコメント
+
+    // 注 : デフォルト引数は末尾から順につけていくこと
+    //int Sud(int a , int b, int c);
+};
+int Calculator::Add(int a,int b)
+{
+    return (a + b);
+}
+
+int Calculator::Mul(int a, int b/*= 2*/)
+{
+    return (a * b);
+}
+
+int Calculator::Sud(int a, int b, int c)
+{
+
+}
+
+int main()
+{
+    Calculator calc;
+    std::cout << calc.Add(10, 20) << std::endl;
+    std::cout << calc.Mul(100,4) << std::endl;
+    std::cout << calc.Mul(100) << std::endl;
+    return 0;
+}
+
