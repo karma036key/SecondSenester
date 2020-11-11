5b4feb9368c3b1ea84ec78507cb0b444d2e8f511
Atsushi-oogami <57586505+karma036key@users.noreply.github.com>
2020-11-11 14:57:33 +0900

    第5回　その３　クラス＋参照 の実践

---
+++ ClassPractice/Practice05/Practice05_03_ClassReference/CharacterBase.h
@@ -0,0 +1,20 @@
+#ifndef CHARACTER_BASE_H
+#define CHARACTER_BASE_H
+
+class CharacterBase
+{
+public:
+	CharacterBase();
+	virtual ~CharacterBase();
+
+	void SetHp(int hp) { m_Hp = hp; }
+	void SetMp(int mp) { m_Mp = mp; }
+
+	int GetHp() const { return m_Hp; }
+	int GetMp() const { return m_Mp; }
+
+	int m_Hp;
+	int m_Mp;
+};
+
+#endif
