d0b30b1e1c366d8205aec208b0fa776f94135954
Atsushi-oogami <57586505+karma036key@users.noreply.github.com>
2020-11-11 18:04:04 +0900

    第5回　その６　名前空間「namespace」
    名前空間作成、動作確認

---
+++ ClassPractice/Practice05/Practice05_06_NameSpace/Enemy.h
@@ -0,0 +1,17 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+// 「namespace」 名前空間の書き方
+//
+//	 namespace namespaceの名前
+//	{
+//			// グローバル変数やクラスの定義
+//	}
+
+
+// enemy名前空間
+namespace Enemy
+{
+	static const int Max = 10;	// 敵最大数
+}
+#endif
---
+++ ClassPractice/Practice05/Practice05_06_NameSpace/Practice05_06_NameSpace.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+#include "Enemy.h"
+#include "Shot.h"
+
+int main()
+{
+    // 「namespace」のデータの使い方
+    // namespace名::データ
+    printf("敵の最大数 = %d\n", Enemy::Max);
+    printf("弾の最大数 = %d\n", Shot::Max);
+
+    std::cout << "std:: も名前空間" << std::endl;
+    return 0;
+}
+
