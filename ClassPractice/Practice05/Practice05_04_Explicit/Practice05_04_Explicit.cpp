7c9f1e95c45a10dbb2bb10a2746d8b323921aaf6
Atsushi-oogami <57586505+karma036key@users.noreply.github.com>
2020-11-11 15:27:30 +0900

    第5回　その4　explicit
    暗黙の型変換について
    変換コンストラクタについて

---
+++ ClassPractice/Practice05/Practice05_04_Explicit/Practice05_04_Explicit.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+
+class A
+{
+public:
+    explicit A(int val)
+    //A(int val) 
+    {
+        std::cout << val << std::endl; 
+    }
+};
+
+class B
+{
+public:
+    explicit B(const A& ref)
+    //B(const A& ref)
+    {
+
+    }
+};
+
+int main()
+{
+    // 引数1つだけとるコンストラクタ ＝ 暗黙の型変換が行われる可能性
+    // 暗黙の型変換のコンストラクタ ＝ 変換コンストラクタ
+    
+/*
+    B b(10000);     // Bクラス のコンストラクタは Aクラスの参照 では？
+                    // 暗黙的に B( A(10000) ) と解釈
+
+    A test = 100;   // Aクラス変数にデータ型代入？
+                    // 暗黙的に A test =A(100)と解釈
+                    // コピーコンストラクタに…
+*/
+
+    A a(35);        // Aクラスのコンストラクタ
+                    // 問題なし
+
+
+
+    return 0;
+}
